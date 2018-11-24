368244e89bf011adf807f180235b12e1a2d4f7be
Reborn <vubao1690@gmail.com>
2018-11-24 07:15:22 +0700

    Login facebook

--- TripBff-Mobile/src/Interfaces.ts
+++ TripBff-Mobile/src/Interfaces.ts
@@ -33,7 +33,8 @@ export module StoreData {
         lastName: string
         fullName: string
 
-        token: string
+        token: string,
+        fbToken: string
         //TODO: expired time...
     }
 
