9e726d670ee0506d8fb084064b398e5f4d500198
Reborn <vubao1690@gmail.com>
2019-05-08 11:58:36 +0700

    allow select/unselect images to be exported

--- TripBff-Mobile/src/screens/trip/preview/index.tsx
+++ TripBff-Mobile/src/screens/trip/preview/index.tsx
@@ -1,28 +1,183 @@
-import React from "react";
-import {  Image } from "react-native";
+import React, { PureComponent } from "react";
+import {  Image, Dimensions, ActivityIndicator } from "react-native";
 import {
-    Container,
-    Header,
-    Content,
-    Button,
-    Icon,
-    Text,
-    View
-  } from "native-base";
-  import { ThunkDispatch } from "redux-thunk";
-  import { PropsBase } from "../../_shared/LayoutContainer";
-  import * as RNa from "react-navigation";
-  import { connect, DispatchProp } from "react-redux";
-  import { tripApi  } from "../../_services/apis";
-  var RNFS = require('react-native-fs');
-  import {
-    ShareDialog,    
-    AccessToken,
-    LoginManager
-  } from "react-native-fbsdk";
-  import { StoreData } from "../../../store/Interfaces";
-  import _, { } from "lodash";
-  import { NavigationConstants } from "../../_shared/ScreenConstants";
+  Container,
+  Header,
+  Content,
+  Button,
+  Icon,
+  Text,
+  View
+} from "native-base";
+import { ThunkDispatch } from "redux-thunk";
+import { PropsBase } from "../../_shared/LayoutContainer";
+import * as RNa from "react-navigation";
+import { connect, DispatchProp } from "react-redux";
+import { tripApi  } from "../../_services/apis";
+var RNFS = require('react-native-fs');
+import {
+  ShareDialog,    
+  AccessToken,
+  LoginManager
+} from "react-native-fbsdk";
+import { StoreData } from "../../../store/Interfaces";
+import _, { } from "lodash";
+import { NavigationConstants } from "../../_shared/ScreenConstants";
+import { TabView } from 'react-native-tab-view';
+import ImageList, { calculateImageListWidth, N_ITEMS_PER_ROW } from "../../../_molecules/ImageList/ImageList";
+import { ImageSelection } from "../../../_molecules/ImageList/ImageSelection";
+import { tripApiService } from "../../../store/ApisAsAService";
+
+interface ImageProps {
+  tripId: string,
+  images?: undefined,
+  updateSelectedImagesUrl: (imageUrls: Array<string>) => void
+}
+
+  class PreviewImagesComponent extends PureComponent<ImageProps, any> {
+      constructor(props) {
+        super(props);
+
+        this.state = {
+            images: []
+        };
+      }
+
+      componentDidMount() {
+        if (this.props.images) {
+          this.setState({ images: this.props.images });
+          let selectedImageUrls = this.props.images.map(item => item.url); 
+          this.props.updateSelectedImagesUrl(selectedImageUrls); 
+        }
+      }
+
+      private _handleSelect(img) {
+        var newImages = this.state.images.map(item => item.imageId != img.imageId ? item : {
+                                            ...item,
+                                            isSelected: !img.isSelected
+                                          });        
+        let selectedImageUrls = newImages.filter(item => item.isSelected).map(item => item.url);    
+
+        this.setState({images: newImages});
+        this.props.updateSelectedImagesUrl(selectedImageUrls);  
+      }
+
+      private renderItem = (itemInfo: { item: any, index: number }) => {
+        const img = itemInfo.item;
+        const { itemWidth } = calculateImageListWidth();
+
+        return (
+            <ImageSelection
+                key={itemInfo.index}
+                imageUrl={img.url}
+                width={itemWidth}
+                isChecked={img.isSelected}
+
+                isFirstItemInRow={itemInfo.index % N_ITEMS_PER_ROW == 0}
+                isFirstRow={ itemInfo.index < N_ITEMS_PER_ROW }
+
+                onPress={() => this._handleSelect(img)}
+            />
+        );
+      }
+
+      render() {
+        return (
+          <View>
+            {
+              this.state.images.length > 0 ?
+                  <ImageList
+                  items={this.state.images}
+                  renderItem={this.renderItem}
+              />
+              : <ActivityIndicator size="small" color="#00ff00" />
+            }
+          </View>
+        );
+      } 
+  }
+
+  const imageMapStateToProps = (storeState: StoreData.BffStoreData, ownProps: ImageProps) => {
+    const { tripId } = ownProps;
+    var trip = _.find(storeState.trips, (item) => item.tripId == tripId);
+    var images = [];
+
+    trip.dates.forEach(date => {
+      date.locations.forEach(location => {
+        images = images.concat(location.images.map(img => {
+          return {
+            ...img,
+            isSelected: true
+          }
+        }));
+      })
+    });    
+
+    return {
+      images: images
+    };
+  };
+  
+  const PreviewImages = connect(
+    imageMapStateToProps,
+    null
+  )(PreviewImagesComponent);
+
+  class PreviewInfographicComponent extends PureComponent<any, any> {
+    constructor(props) {
+      super(props);
+
+      this.state = { 
+        imageUri: ""
+      };
+    }
+
+    componentDidMount() {
+      this._getInfographic();
+    }   
+
+    private _getInfographic = () => {
+      console.log('tripId: ' + this.props.tripId);
+
+        tripApi
+        .get(`/trips/${this.props.tripId}/infographics/${this.props.infographicId}`)
+        .then(res => {
+            if (res.data) {
+              this.setState({imageUri: res.data});     
+            
+              var path = RNFS.DocumentDirectoryPath + '/test1.png';
+  
+              // write the file
+              RNFS.writeFile(path, res.data, 'base64')
+              .then((success) => {
+                  console.log('FILE WRITTEN!');
+  
+                  const photoUri = "file://" + path;
+                  this.props.updateShareInfographicUrl(photoUri);
+              })
+              .catch((err) => {
+                  console.log(err.message);
+              });          
+            }            
+        })
+        .catch(error => {
+            console.log("error: " + JSON.stringify(error));
+        });
+    }
+
+    render() {
+      return (
+        <View>
+            <Image
+                source={{
+                  uri: 'data:image/png;base64,' + this.state.imageUri             
+                }}
+                style={{width: 400, height: 600}}
+              />  
+        </View>
+      );
+    }
+  }
 
   export interface Props extends IMapDispatchToProps, DispatchProp, PropsBase {
     dispatch: ThunkDispatch<any, null, any>;
@@ -31,123 +186,112 @@ import {
     infographicId: string
   }
   
-  interface IMapDispatchToProps {
-    
+  interface IMapDispatchToProps {    
   }
 
   interface State {
-    isLoaded: boolean,
-    imageUri: string,
-    sharePhotoContent: any
+    index: number,
+    routes: any,
+    infographicUrl: string
+    imageUrls: Array<string>
   } 
 
-  class InfographicPreview extends React.Component<Props, State> {
+  class InfographicPreview extends React.PureComponent<Props, State> {
     constructor(props) {
       super(props);
 
-      const sharePhotoContent = {
-        contentType: "photo",
-        photos: []
-      };
+      this.state = {
+        index: 0,
+        routes: [
+          { key: 'first', title: 'Infographic' },
+          { key: 'second', title: 'Images' },
+        ],
+        infographicUrl: "",
+        imageUrls: []
+      }
+    } 
 
-      this.state = { 
-            isLoaded: false,
-            imageUri: "",
-            sharePhotoContent: sharePhotoContent
-      };
+    private _updateShareInfographicUrl = (imageUrl) => {
+      this.setState({infographicUrl: imageUrl});
     }
 
-    componentDidMount() {
-        this._getInfographic();
-    }
-
-    private _getInfographic = () => {
-      console.log('tripId: ' + this.props.tripId);
-
-        tripApi
-        .get(`/trips/${this.props.tripId}/infographics/${this.props.infographicId}`)
-        .then(res => {
-            this.setState({imageUri: res.data});     
-            
-            var path = RNFS.DocumentDirectoryPath + '/test1.png';
-
-            // write the file
-            RNFS.writeFile(path, res.data, 'base64')
-            .then((success) => {
-                console.log('FILE WRITTEN!');
-
-                const photoUri = "file://" + path;
-                const sharePhotoContent = {
-                contentType: "photo",
-                photos: [
-                    { imageUrl: photoUri }
-                ]
-                };
-
-                this.setState({sharePhotoContent: sharePhotoContent});
-
-            })
-            .catch((err) => {
-                console.log(err.message);
-            });          
-        })
-        .catch(error => {
-            console.log("error: " + JSON.stringify(error));
-        });
+    private _updateSelectedImagesUrl = (imageUrls) => {     
+      this.setState({imageUrls: imageUrls});
     }
 
     private _sharePhotoWithShareDialog = () => {
-        var tmp = this;
+       var tmp = this;
+       let imageUrls = [this.state.infographicUrl].concat(this.state.imageUrls);        
 
-        AccessToken.getCurrentAccessToken().then(
-            (data) => {
-              if (data) {
-                ShareDialog.canShow(this.state.sharePhotoContent)
-                  .then(function(canShow) {
-                    if (canShow) {
-                      return ShareDialog.show(tmp.state.sharePhotoContent);
+       let photos = imageUrls.map(item => {
+         return {  imageUrl: item }
+       });
+
+      const sharePhotoContent = {
+        contentType: "photo",
+        photos: photos
+      } as any;
+
+      AccessToken.getCurrentAccessToken().then(
+          (data) => {
+            if (data) {
+              try{
+                ShareDialog.canShow(sharePhotoContent)
+                .then(function(canShow) {
+                  if (canShow) {
+                    return ShareDialog.show(sharePhotoContent)                    
+                  }
+                })
+                .then(
+                  function(result) {
+                    console.log("Share result: " + JSON.stringify(result));
+                    if (result.isCancelled) {
+                      console.log("Share cancelled");
+                    } else {
+                      console.log("Share success");
+                      this._navigateToProfile();
                     }
-                  })
-                  .then(
-                    function(result) {
-                      if (result.isCancelled) {
-                        console.log("Share cancelled");
-                      } else {
-                        console.log("Share success");
-                      }
-                    },
-                    function(error) {
-                      console.log("Share fail with error: " + error);
-                    }
-                  );
+                  },
+                  function(error) {
+                    console.log("Share fail with error: " + error);
+                  }
+                );
               }
-              else {
-                  console.log('need to log-in');
-                  LoginManager.logInWithReadPermissions(["public_profile", "user_photos", "user_posts"]).then(
-                    function(result) {
-                      if (result.isCancelled) {
-                        console.log("Login cancelled");
-                      } else {
-                        console.log(
-                          "Login success with permissions: " +
-                            result.grantedPermissions.toString()
-                        );
-                        tmp._sharePhotoWithShareDialog();
-                      }
-                    },
-                    function(error) {
-                      console.log("Login fail with error: " + error);
-                    }
-                  );
+              catch(error) {
+                  console.log('come here error try catch');
               }
-            } 
+              
+            }
+            else {
+                console.log('need to log-in');
+                LoginManager.logInWithReadPermissions(["public_profile", "user_photos", "user_posts"]).then(
+                  function(result) {
+                    if (result.isCancelled) {
+                      console.log("Login cancelled");
+                    } else {
+                      console.log(
+                        "Login success with permissions: " +
+                          result.grantedPermissions.toString()
+                      );
+                      tmp._sharePhotoWithShareDialog();
+                    }
+                  },
+                  function(error) {
+                    console.log("Login fail with error: " + error);
+                  }
+                );
+            }
+          } 
         );    
       }
 
     private _cancel = () => {
-      this.props.navigation.navigate(NavigationConstants.Screens.Profile)
+      this._navigateToProfile();
     }
 
+    private _navigateToProfile = () => {
+      this.props.navigation.navigate(NavigationConstants.Screens.Profile)
+    }
     render() {
         return (
           <Container>
@@ -172,15 +316,27 @@ import {
             </Header>
             <Content>
               <View>             
-                <Text style={{ margin: 10 }}>Preview infographic:</Text>
-                <Image
-                    source={{
-                      uri: 'data:image/png;base64,' + this.state.imageUri             
-                    }}
-                    style={{width: 400, height: 600}}
-                  />  
-
-              </View>
+                    <TabView
+                      navigationState={this.state}
+                      renderScene={({ route }) => {
+                        switch (route.key) {
+                          case 'first':
+                            return <PreviewInfographicComponent tripId={this.props.tripId}
+                                      infographicId={this.props.infographicId}
+                                      updateShareInfographicUrl={this._updateShareInfographicUrl}>
+                                  </PreviewInfographicComponent>;
+                          case 'second':
+                            return <PreviewImages tripId={this.props.tripId}
+                                      updateSelectedImagesUrl={this._updateSelectedImagesUrl}>                              
+                                  </PreviewImages>;
+                          default:
+                            return null;
+                        }
+                      }}
+                      onIndexChange={index => this.setState({ index })}
+                      initialLayout={{ width: Dimensions.get('window').width }}
+                    />
+              </View>              
             </Content>            
           </Container>
         );
@@ -195,7 +351,7 @@ import {
     var trip = _.find(storeState.trips, (item) => item.tripId == tripId)
     return {
       tripId: tripId,
-      infographicId: trip.infographicId,
+      infographicId: trip.infographicId
     };
   };
 
