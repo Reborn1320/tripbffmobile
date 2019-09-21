10af93fce4f475faa733751cdcabd6b0806371be
TRUONG Ngoc Tuan <tuan.truongngoc@pycogroup.com>
2019-09-21 13:42:41 +0700

    testing linked to terraform infra

--- TripBffMobileV2/src/screens/_services/ServiceConstants.ts
+++ TripBffMobileV2/src/screens/_services/ServiceConstants.ts
@@ -1,5 +1,8 @@
-export var TRIP_URL = "http://192.168.42.236:8000"
-export var SSO_URL = "http://192.168.42.236:3000"
+// export var TRIP_URL = "http://192.168.42.236:8000";
+// export var SSO_URL = "http://192.168.42.236:3000";
 
-// export var TRIP_URL = "https://api.tripbff.com/tripApi/prod/v1"
-// export var SSO_URL = "https://api.tripbff.com/ssoApi/prod/v1"
+export var TRIP_URL = "http://trip-api.tntuan.tk";
+export var SSO_URL = "http://sso.tntuan.tk";
+
+// export var TRIP_URL = "https://api.tripbff.com/tripApi/prod/v1";
+// export var SSO_URL = "https://api.tripbff.com/ssoApi/prod/v1";
--- TripBffMobileV2/src/screens/login/index.tsx
+++ TripBffMobileV2/src/screens/login/index.tsx
@@ -1,31 +1,35 @@
 import React, { Component } from "react";
-import { View, Button, Text, Icon } from 'native-base';
-import { ImageBackground, ViewStyle , StyleSheet, ImageStyle, TextStyle} from "react-native";
-import { connect } from "react-redux";
+import { View, Button, Text, Icon, Toast } from "native-base";
 import {
-  AccessToken,
-  LoginManager
-} from "react-native-fbsdk";
-import * as RNa from "react-navigation";
-import { loginUsingFacebookAccessToken, loginUsingDeviceId } from "../../store/User/operations";
+  ImageBackground,
+  ViewStyle,
+  StyleSheet,
+  ImageStyle,
+  TextStyle,
+} from "react-native";
+import { connect } from "react-redux";
+import { AccessToken, LoginManager } from "react-native-fbsdk";
+import {
+  loginUsingFacebookAccessToken,
+  loginUsingDeviceId,
+} from "../../store/User/operations";
 import { NavigationConstants } from "../_shared/ScreenConstants";
 import { mixins } from "../../_utils";
-import { withNamespaces } from 'react-i18next';
+import { withNamespaces } from "react-i18next";
 import { PropsBase } from "../_shared/LayoutContainer";
 
-export interface Props extends PropsBase {
-}
+export interface Props extends PropsBase {}
 
 interface IMapDispatchToProps {
-  loginUsingFacebookAccessToken: (userId, accessToken) => Promise<void>
-  loginUsingDeviceId: () => Promise<void>
+  loginUsingFacebookAccessToken: (userId, accessToken) => Promise<void>;
+  loginUsingDeviceId: () => Promise<void>;
 }
 
-class Login extends Component<Props & IMapDispatchToProps, any>{
+class Login extends Component<Props & IMapDispatchToProps, any> {
   private _loginFacebook = () => {
-    var tmp =  this;
+    var tmp = this;
 
-    LoginManager.logInWithPermissions(["public_profile", 'email']).then(
+    LoginManager.logInWithPermissions(["public_profile", "email"]).then(
       function(result) {
         if (result.isCancelled) {
           console.log("Login cancelled");
@@ -33,7 +37,7 @@ class Login extends Component<Props & IMapDispatchToProps, any>{
           AccessToken.getCurrentAccessToken().then(data => {
             //console.log(data.accessToken.toString());
             console.log("getCurrentAccessToken data", data);
-    
+
             tmp._loginFacebookAccess(data.userID, data.accessToken);
           });
         }
@@ -42,57 +46,86 @@ class Login extends Component<Props & IMapDispatchToProps, any>{
         console.log("Login fail with error: " + error);
       }
     );
-  }
+  };
 
-  private _loginFacebookAccess = (facebookUserId, accessToken, isMoveToCreate = true) => {
-    return this.props.loginUsingFacebookAccessToken(facebookUserId, accessToken)
+  private _loginFacebookAccess = (
+    facebookUserId,
+    accessToken,
+    isMoveToCreate = true
+  ) => {
+    return this.props
+      .loginUsingFacebookAccessToken(facebookUserId, accessToken)
       .then(() => {
         if (isMoveToCreate) {
           this.props.navigation.navigate(NavigationConstants.Screens.Profile);
         }
+      })
+      .catch(() => {
+        Toast.show({
+          text: "Cannot perform facebook login",
+        });
       });
-  }  
+  };
 
-  private _loginUniqueDevice = async (isMoveToCreate = true) => {    
-    this.props.loginUsingDeviceId()
+  private _loginUniqueDevice = async (isMoveToCreate = true) => {
+    this.props
+      .loginUsingDeviceId()
       .then(() => {
         if (isMoveToCreate) {
           this.props.navigation.navigate(NavigationConstants.Screens.Profile);
         }
+      })
+      .catch(() => {
+        Toast.show({
+          text: "Cannot continue without login",
+        });
       });
-  }
+  };
 
   render() {
     const { t, i18n } = this.props;
 
     return (
       <View>
-          <ImageBackground source={require('../../../assets/04_background.jpg')} style={styles.imageBackground}>              
-          </ImageBackground>
+        <ImageBackground
+          source={require("../../../assets/04_background.jpg")}
+          style={styles.imageBackground}
+        ></ImageBackground>
 
-          <View style={[styles.overlay]} />
-          
-          <View style={styles.loginContainer}>
-            <View style={styles.welcomeContainer}>
-              <Text style={styles.welcomeTitle}>{t("login:welcome_title")}</Text>
-            </View>
-            <View style={styles.buttonsContainer}>
-              <Button
-                iconLeft
-                onPress={this._loginFacebook}
-                style={styles.facebookButton}              
-              >
-                <Icon name='facebook-f' type="FontAwesome5" style={styles.facebookIcon}/> 
-                <Text style={styles.buttonTitle}>{t("login:facebook_button_title")}</Text>
-              </Button>             
+        <View style={[styles.overlay]} />
 
-              <Button style={styles.noLoginButton}
-                dark onPress={this._loginUniqueDevice}>
-                <Text style={styles.buttonTitle}>{t("login:no_login_button_title")}</Text>
-              </Button>
-            </View>
+        <View style={styles.loginContainer}>
+          <View style={styles.welcomeContainer}>
+            <Text style={styles.welcomeTitle}>{t("login:welcome_title")}</Text>
+          </View>
+          <View style={styles.buttonsContainer}>
+            <Button
+              iconLeft
+              onPress={this._loginFacebook}
+              style={styles.facebookButton}
+            >
+              <Icon
+                name="facebook-f"
+                type="FontAwesome5"
+                style={styles.facebookIcon}
+              />
+              <Text style={styles.buttonTitle}>
+                {t("login:facebook_button_title")}
+              </Text>
+            </Button>
+
+            <Button
+              style={styles.noLoginButton}
+              dark
+              onPress={this._loginUniqueDevice}
+            >
+              <Text style={styles.buttonTitle}>
+                {t("login:no_login_button_title")}
+              </Text>
+            </Button>
+          </View>
         </View>
-      </View>      
+      </View>
     );
   }
 }
@@ -112,71 +145,75 @@ interface Style {
 
 const styles = StyleSheet.create<Style>({
   imageBackground: {
-    width: '100%',
-    height: '100%'
+    width: "100%",
+    height: "100%",
   },
   loginContainer: {
     flex: 1,
-    position: 'absolute',
+    position: "absolute",
     left: 0,
     top: 0,
-    width: '100%',
-    height: '100%'
+    width: "100%",
+    height: "100%",
   },
   welcomeContainer: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
   },
   welcomeTitle: {
     color: "white",
-    textAlign: 'center',
+    textAlign: "center",
     ...mixins.themes.fontNormal,
-    fontSize: 44
+    fontSize: 44,
   },
   buttonsContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
   },
   facebookButton: {
     margin: 5,
     alignSelf: "center",
     justifyContent: "center",
     backgroundColor: "#4267B2",
-    width: "80%"
+    width: "80%",
   },
   facebookIcon: {
-    fontSize: 16
+    fontSize: 16,
   },
   noLoginButton: {
-    margin: 5,              
+    margin: 5,
     alignSelf: "center",
     justifyContent: "center",
-    width: "80%"
+    width: "80%",
   },
   buttonTitle: {
-    ...mixins.themes.fontNormal
+    ...mixins.themes.fontNormal,
   },
   overlay: {
     flex: 1,
-    position: 'absolute',
+    position: "absolute",
     left: 0,
     top: 0,
     opacity: 0.2,
-    backgroundColor: 'black',
+    backgroundColor: "black",
     width: "100%",
-    height: "100%"
-  }  
-})
+    height: "100%",
+  },
+});
 
 const mapDispatchToProps = (dispatch): IMapDispatchToProps => {
   return {
-    loginUsingFacebookAccessToken: (userId, accessToken) => dispatch(loginUsingFacebookAccessToken(userId, accessToken, "")),
+    loginUsingFacebookAccessToken: (userId, accessToken) =>
+      dispatch(loginUsingFacebookAccessToken(userId, accessToken, "")),
     loginUsingDeviceId: () => dispatch(loginUsingDeviceId()),
   };
 };
 
-const LoginScreen = connect(null, mapDispatchToProps)(Login);
+const LoginScreen = connect(
+  null,
+  mapDispatchToProps
+)(Login);
 
-export default withNamespaces(['login'])(LoginScreen);
+export default withNamespaces(["login"])(LoginScreen);
