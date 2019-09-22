b6b4edf685d1b58bfe593ad32e97bc4493f4f908
TRUONG Ngoc Tuan <tuan.truongngoc@pycogroup.com>
2019-09-22 10:32:26 +0700

    format screens

--- TripBffMobileV2/src/screens/trip/create/index.tsx
+++ TripBffMobileV2/src/screens/trip/create/index.tsx
@@ -1,50 +1,63 @@
 import React, { Component } from "react";
-import { Container, Content } from 'native-base';
+import { Container, Content } from "native-base";
 import { StoreData } from "../../../store/Interfaces";
 import { connect } from "react-redux";
 import { Moment } from "moment";
 import { PropsBase } from "../../_shared/LayoutContainer";
 import TripCreationForm from "./TripCreationForm";
-import { createTrip as createTripAsync, updateTrip } from "../../../store/Trip/operations";
+import {
+  createTrip as createTripAsync,
+  updateTrip,
+} from "../../../store/Trip/operations";
 import { NavigationConstants } from "../../_shared/ScreenConstants";
 import { withNamespaces } from "react-i18next";
 
 export interface Props extends IMapDispatchToProps, PropsBase {
-  user: StoreData.UserVM
+  user: StoreData.UserVM;
 }
 
 interface IMapDispatchToProps {
-  createTripAsync: (name: string, fromDate: Moment, toDate: Moment) => Promise<string>;
-  updateTrip: (tripId: string, name: string, fromDate: Moment, toDate: Moment) => Promise<any>;
+  createTripAsync: (
+    name: string,
+    fromDate: Moment,
+    toDate: Moment
+  ) => Promise<string>;
+  updateTrip: (
+    tripId: string,
+    name: string,
+    fromDate: Moment,
+    toDate: Moment
+  ) => Promise<any>;
 }
 
-interface State {
-}
+interface State {}
 
 class TripCreation extends Component<Props, State> {
-
   static navigationOptions = ({ navigation, screenProps }) => {
     return {
-      title:  screenProps.t('create:screen_header_title')
+      title: screenProps.t("create:screen_header_title"),
     };
-  };    
+  };
 
   private _onCreatedOrUpdatedHandler = (tripId, tripName) => {
     this.props.navigation.navigate(NavigationConstants.Screens.TripImport, {
-        tripId: tripId,
-        otherParam: tripName });
-  }
+      tripId: tripId,
+      otherParam: tripName,
+    });
+  };
 
   render() {
     return (
       <Container>
         <Content>
-          <TripCreationForm createTrip={this.props.createTripAsync} 
-                            updateTrip={this.props.updateTrip}
-                            onTripCreatedUpdatedHandler={this._onCreatedOrUpdatedHandler}
-                            titleButton={"action:next"} 
-                            navigation={this.props.navigation} />
-        </Content>        
+          <TripCreationForm
+            createTrip={this.props.createTripAsync}
+            updateTrip={this.props.updateTrip}
+            onTripCreatedUpdatedHandler={this._onCreatedOrUpdatedHandler}
+            titleButton={"action:next"}
+            navigation={this.props.navigation}
+          />
+        </Content>
       </Container>
     );
   }
@@ -52,11 +65,16 @@ class TripCreation extends Component<Props, State> {
 
 const mapDispatchToProps = dispatch => {
   return {
-    createTripAsync: (name, fromDate, toDate) => dispatch(createTripAsync(name, fromDate, toDate)),
-    updateTrip: (tripId, name, fromDate, toDate) => dispatch(updateTrip(tripId, name, fromDate, toDate))
-  }
+    createTripAsync: (name, fromDate, toDate) =>
+      dispatch(createTripAsync(name, fromDate, toDate)),
+    updateTrip: (tripId, name, fromDate, toDate) =>
+      dispatch(updateTrip(tripId, name, fromDate, toDate)),
+  };
 };
 
-const TripCreationScreen = connect(null, mapDispatchToProps)(TripCreation);
+const TripCreationScreen = connect(
+  null,
+  mapDispatchToProps
+)(TripCreation);
 
-export default withNamespaces(['create', 'action'])(TripCreationScreen);
+export default withNamespaces(["create", "action"])(TripCreationScreen);
--- TripBffMobileV2/src/screens/user/ProfileScreenContainer.ts
+++ TripBffMobileV2/src/screens/user/ProfileScreenContainer.ts
@@ -1,18 +1,22 @@
 import { connect } from "react-redux";
 import _ from "lodash";
-import { fetchTrips, deleteTrip, getCurrentMinimizedTrip } from "../../store/Trips/operations";
+import {
+  fetchTrips,
+  deleteTrip,
+  getCurrentMinimizedTrip,
+} from "../../store/Trips/operations";
 import { addTrips } from "../../store/Trips/actions";
-import  ProfileScreen  from "./ProfileScreen";
-import { StoreData } from "../../store/Interfaces";
+import ProfileScreen from "./ProfileScreen";
 import { clearAllDatasource } from "../../store/DataSource/actions";
 
 const mapDispatchToProps = dispatch => {
   return {
     fetchTrips: (cancelToken: any) => dispatch(fetchTrips(cancelToken)),
-    addTrips: (trips) => dispatch(addTrips(trips)),
-    deleteTrip: (tripId) => dispatch(deleteTrip(tripId)),
-    getCurrentMinimizedTrip: (tripId) => dispatch(getCurrentMinimizedTrip(tripId)),
-    clearDatasource: () => dispatch(clearAllDatasource())
+    addTrips: trips => dispatch(addTrips(trips)),
+    deleteTrip: tripId => dispatch(deleteTrip(tripId)),
+    getCurrentMinimizedTrip: tripId =>
+      dispatch(getCurrentMinimizedTrip(tripId)),
+    clearDatasource: () => dispatch(clearAllDatasource()),
   };
 };
 
