e5c59307393c3592073f964bbe07d06ba6e9e7a0
TRUONG Ngoc Tuan <tuan.truongngoc@pycogroup.com>
2019-10-13 11:06:50 +0700

    update latest build version + url server

--- TripBffMobileV2/src/screens/_services/ServiceConstants.ts
+++ TripBffMobileV2/src/screens/_services/ServiceConstants.ts
@@ -1,8 +1,12 @@
-export var TRIP_URL = "http://192.168.42.236:8000";
-export var SSO_URL = "http://192.168.42.236:3000";
+// export var TRIP_URL = "http://192.168.1.5:8000";
+// export var SSO_URL = "http://192.168.1.5:3000";
 
 // export var TRIP_URL = "http://trip-api.tntuan.tk";
 // export var SSO_URL = "http://sso.tntuan.tk";
+
+const posfix = "-prod.tripbff.com";
+export var TRIP_URL = `http://trip-api${posfix}`;
+export var SSO_URL = `http://sso${posfix}`;
 
 // export var TRIP_URL = "https://api.tripbff.com/tripApi/prod/v1";
 // export var SSO_URL = "https://api.tripbff.com/ssoApi/prod/v1";
